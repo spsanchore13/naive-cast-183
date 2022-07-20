0abbf93f785daf2f35358475740fecbfb8fae60a
spsanchore13 <shantilalpatliya18@gmail.com>
2022-07-21 00:22:26 +0530

    Navbar

---
+++ component/navbar.js
@@ -0,0 +1,95 @@
+let navbar = () => {
+    return `<div id="topbar">
+    <img src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png" alt="pharmeasy.in">
+    <p>Select Pincode
+        <ion-icon name="chevron-down-outline"></ion-icon>
+    </p>
+    <div id="container">
+    <div id="searchBox">
+        <input type="text" placeholder="Search Medicines/Healthcare Products" id="search">
+        <ion-icon name="search-outline" class="search-icon"></ion-icon>
+    </div>
+    <div id="suggestion"></div>
+    </div>
+</div>
+
+<div id="bottombar">
+    <div>
+        <p>
+            <a href="">Order Medicines</a>
+        </p>
+        <p>
+            <a href="">Healthcare Products</a>
+        </p>
+        <p>
+            <a href="">Lab Tests</a>
+        </p>
+        <p>
+            <a href="">RTPCR</a>
+        </p>
+    </div>
+    <div>
+        <p class="open-btn">
+            <ion-icon name="person-outline"></ion-icon>
+            Log in
+        </p>
+
+        <div class="form-popup" id="myForm">
+            <form class="login-form">
+                <div class="form-top">
+                    <img src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png?dim=360x0" alt="">
+                    <ion-icon name="close-outline" class="close"></ion-icon>
+                </div>
+
+                <div>
+                    <h1>Login</h1>
+
+                    <input type="text" placeholder="Username" id="username" required>
+                    <input type="password" placeholder="Password" id="password" required>
+
+                    <button type="submit" class="btn">Login</button>
+                </div>
+
+            </form>
+        </div>
+
+        <p class="open-btn-signup">
+            <ion-icon name="person-outline"></ion-icon>
+            Sign Up
+        </p>
+
+        <div class="form-popup-signup" id="signUp">
+            <form class="signup-form">
+                <div class="signup-form-top">
+                    <img src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png?dim=360x0" alt="">
+                    <ion-icon name="close-outline" class="close-btn-signup"></ion-icon>
+                </div>
+
+                <div>
+                    <h1>Sign Up</h1>
+                    <input type="text" id="signup_name" placeholder="Name">
+                    <input type="email" placeholder="Email" id="signup_email">
+                    <input type="number" id="signup_mobile" placeholder="Mobile">
+                    <input type="text" id="signup_username" placeholder="Username">
+                    <input type="password" placeholder="Password" id="signup_password">
+                    <input type="text" id="signup_description" placeholder="Description">
+                    <button type="submit" class="btn-signup">Sign Up</button>
+                </div>
+
+            </form>
+        </div>
+
+
+        <p><a href="">
+                <ion-icon name="flower-outline"></ion-icon>
+                Offers
+            </a></p>
+        <p><a href="">
+                <ion-icon name="cart-outline"></ion-icon>
+                Cart
+            </a></p>
+    </div>
+</div>`
+}
+
+export default navbar;
---
+++ script/navbar.js
@@ -0,0 +1,109 @@
+import navbar from "../component/navbar.js";
+
+document.querySelector('#navbar').innerHTML = navbar();
+
+document.querySelector(".open-btn").addEventListener("click", openForm)
+document.querySelector(".close").addEventListener("click", closeForm)
+document.querySelector(".open-btn-signup").addEventListener("click", openFormSignUp)
+document.querySelector(".close-btn-signup").addEventListener("click", closeFormSignUp)
+
+
+//Login Form Function
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// Sign Form Function
+function openFormSignUp() {
+    document.getElementById("signUp").style.display = "block";
+}
+
+function closeFormSignUp() {
+    document.getElementById("signUp").style.display = "none";
+}
+
+let suggestion = () => {
+    let res = document.querySelector("#suggestion");
+    let p = document.createElement("p");
+    pp.innerText = "Patliya"
+    res.append(p, p1)
+}
+
+
+let submit_query = () => {
+    let q = document.querySelector("#search").value;
+    console.log('q:', q)
+}
+document.querySelector(".search-icon").addEventListener("click", submit_query);
+
+
+document.querySelector(".btn-signup").addEventListener("click", userData);
+
+async function userData(event) {
+    event.preventDefault();
+    let name = document.querySelector("#signup_name").value;
+    let email = document.querySelector("#signup_email").value;
+    let mobile = document.querySelector("#signup_mobile").value;
+    let username = document.querySelector("#signup_username").value;
+    let password = document.querySelector("#signup_password").value;
+    let description = document.querySelector("#signup_description").value;
+    let obj = { name, email, mobile, username, password, description }
+    let data = JSON.stringify(obj);
+
+    let response = await fetch("https://masai-api-mocker.herokuapp.com/auth/register", {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+
+    let res = await response.json();
+    alert(res.message)
+};
+
+document.querySelector(".btn").addEventListener("click", login)
+async function login(event) {
+    event.preventDefault()
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+    let obj = { username, password }
+
+    obj = JSON.stringify(obj)
+
+    let response = await fetch("https://masai-api-mocker.herokuapp.com/auth/login", {
+        method: "POST",
+        body: obj,
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+
+    let res = await response.json();
+
+    if (res.error) {
+        alert(res.message)
+    } else {
+        alert("Login Successfull")
+        getUserDetail(username, res.token)
+        localStorage.setItem("system", "online")
+        // window.location.reload();
+    }
+}
+
+let getUserDetail = async (username, token) => {
+    let url = `https://masai-api-mocker.herokuapp.com/user/${username}`
+    let response = await fetch(url, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    let data = await response.json();
+    localStorage.setItem("user", JSON.stringify(data))
+}
